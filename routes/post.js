ecd057c6e0307c1ecf6d4377f8570a77ab71bc06
aung <aungnyeinchann416@gmail.com>
2024-06-26 12:02:00 +0630

    Node Startup

---
+++ routes/post.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.json({ msg: "All Posts" });
+});
+
+router.post("/", (req, res) => {
+  res.json(req.body);
+});
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.json({ msg: "Get post id is " + req.params.id });
+  })
+  .patch((req, res) => {
+    res.json({ msg: "Edit id is " + req.params.id });
+  })
+  .delete((req, res) => {
+    res.json({ msg: "Delete id is " + req.params.id });
+  });
+
+// router.get("/:id", (req, res) => {
+//   res.json({ msg: "Get post id is " + req.params.id });
+// });
+
+// router.patch("/:id", (req, res) => {
+//   res.json({ msg: "Edit id is " + req.params.id });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   res.json({ msg: "Delete id is " + req.params.id });
+// });
+
+module.exports = router;
